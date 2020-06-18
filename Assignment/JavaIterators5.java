82c28437a5bdf74a687d67fd0c6c53e99651ab93
Muonaka Anthony <Antonynans@gmail.com>
2020-06-18 07:27:42 +0100

    Iteration problems in Java

--- Assignment/JavaIterators5.java
+++ Assignment/JavaIterators5.java
@@ -1,3 +1,10 @@
+//
+//	Write a do-while loop that asks the user to enter two numbers. The
+//	numbers should be added and the sum displayed. The loop should ask the
+//	user whether he or she wishes to perform the operation again. If so, the
+//	loop should repeat; otherwise, it should terminate.
+
+
 import java.util.Scanner;
 public class JavaIterators5{
 	public static void main(String[] args) {
