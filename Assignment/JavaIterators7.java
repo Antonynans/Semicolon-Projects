375e8c8758fa0601b65add8bf829469c540fbd2a
Muonaka Anthony <Antonynans@gmail.com>
2020-06-18 07:29:47 +0100

    Iteration problems in Java

--- Assignment/JavaIterators7.java
+++ Assignment/JavaIterators7.java
@@ -1,3 +1,11 @@
+//
+//	Write a program that prompts the user to input an integer and then
+//	outputs the number with the digits reversed. For example, if the input is
+//	12345, the output should be 54321.
+//
+	
+	
+
 import java.util.Scanner;
 public class JavaIterators7{
 	public static void main(String[] args) {
